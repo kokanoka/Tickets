60446e21c22e2261138465611fd81fa51a8e08e3
kokanoka <kokanoka30@gmail.com>
2021-09-29 20:14:36 +0300

    add Ticket.java, TicketRepository.java, TicketManager.java, TicketManagerTest.java

--- src/main/java/ru/netology/domain/Ticket.java
+++ src/main/java/ru/netology/domain/Ticket.java
@@ -1,5 +1,22 @@
 package ru.netology.domain;
 
-public class Ticket {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class Ticket implements Comparable<Ticket> {
+    private int id;
+    private int price;
+    private String departure;
+    private String arrival;
+    private int time;
+
+    @Override
+    public int compareTo(Ticket o) {
+        return this.price - o.price;
+    }
 }
---
+++ src/test/java/ru/netology/ru/netology/manager/TicketManagerTest.java
@@ -0,0 +1,42 @@
+package ru.netology.ru.netology.manager;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Ticket;
+import ru.netology.repository.TicketRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicketManagerTest {
+    private TicketRepository repository = new TicketRepository();
+    private TicketManager manager = new TicketManager(repository);
+
+    private Ticket first = new Ticket(1, 2399, "KZN", "UFA", 120);
+    private Ticket second = new Ticket(45, 2568, "DME", "UFA", 240);
+    private Ticket third = new Ticket(36, 5783, "DME", "KZN", 190);
+    private Ticket fourth = new Ticket(28, 1476, "KZN", "UFA", 120);
+    private Ticket fifth = new Ticket(87, 3672, "KZN", "UFA", 120);
+
+    @BeforeEach
+    public void setUp() {
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+    }
+
+    @Test
+    void shouldFindAll() {
+        Ticket[] actual = manager.findAll("KZN", "UFA");
+        Ticket[] expected = {fourth, first, fifth};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFindNoTicket() {
+        Ticket[] actual = manager.findAll("KZN", "LED");
+        Ticket[] expected = {};
+        assertArrayEquals(expected, actual);
+    }
+}
